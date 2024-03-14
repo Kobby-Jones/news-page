88893dfe3e3d7887bfc36ed4a8993f3560b63084
Kobby Jones <kobbyj099@gmail.com>
2024-03-14 01:34:53 +0000

    Modified

--- js/script1.js
+++ js/script1.js
@@ -20,15 +20,23 @@ function create_News_Card(data) {
     cardImage.classList.add("card-img-top");
     cardImage.src = data.image_url;
     newsCard.appendChild(cardImage);
-    const cardTitle = document.createElement("h2");
-    cardTitle.classList.add("card-title");
+    const cardTitle = document.createElement("h3");
+    cardTitle.classList.add("card-title", "text-light");
     cardTitle.innerText = data.title;
-    newsCard.appendChild(cardTitle)
-    const cardText = document.createElement("p")
-    cardText.classList.add("card-text");
-    cardText.innerText = data.summary.substring(0, 70) + "...";
+    newsCard.appendChild(cardTitle);
+    const cardText = document.createElement("p");
+    cardText.classList.add("card-text", "text-light");
+    cardText.innerText = data.summary.substring(0, 100) + "...";
     newsCard.appendChild(cardText)
-    col.appendChild(newsCard)
-    homeFeed.appendChild(col)
+    const readMore_url = document.createElement("a");
+    readMore_url.href = data.url;
+    readMore_url.target = "_blank";
+    const button = document.createElement("button");
+    button.classList.add("btn", "btn-secondary", "rounded-3");
+    button.innerText = "Read More";
+    readMore_url.appendChild(button);
+    newsCard.appendChild(readMore_url);
+    col.appendChild(newsCard);
+    homeFeed.appendChild(col);
     
 }
